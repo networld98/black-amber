24ef640f0557a1e256f86864f892b83087ca0b8e
netwo <stercenko.sergej@gmail.com>
2024-04-10 14:44:57 +0300

    Переделал функционал подарков для работы в малой корзине, доработал менюхи для работы с ЧПУ

--- www/catalog/controller/common/cart.php
+++ www/catalog/controller/common/cart.php
@@ -1,143 +1,165 @@
 <?php
 class ControllerCommonCart extends Controller {
-	public function index() {
-		$this->load->language('common/cart');
+    public function index() {
+        $this->load->model('extension/gift_price/gift');
+        $this->load->language('common/cart');
 
-		// Totals
-		$this->load->model('setting/extension');
+        // Totals
+        $this->load->model('setting/extension');
 
-		$totals = array();
-		$taxes = $this->cart->getTaxes();
-		$total = 0;
+        $totals = array();
+        $taxes = $this->cart->getTaxes();
+        $total = 0;
 
-		// Because __call can not keep var references so we put them into an array.
-		$total_data = array(
-			'totals' => &$totals,
-			'taxes'  => &$taxes,
-			'total'  => &$total
-		);
-			
-		// Display prices
-		if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
-			$sort_order = array();
+        // Because __call can not keep var references so we put them into an array.
+        $total_data = array(
+            'totals' => &$totals,
+            'taxes'  => &$taxes,
+            'total'  => &$total
+        );
 
-			$results = $this->model_setting_extension->getExtensions('total');
 
-			foreach ($results as $key => $value) {
-				$sort_order[$key] = $this->config->get('total_' . $value['code'] . '_sort_order');
-			}
+        // Display prices
+        if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
+            $sort_order = array();
 
-			array_multisort($sort_order, SORT_ASC, $results);
+            $results = $this->model_setting_extension->getExtensions('total');
 
-			foreach ($results as $result) {
-				if ($this->config->get('total_' . $result['code'] . '_status')) {
-					$this->load->model('extension/total/' . $result['code']);
+            foreach ($results as $key => $value) {
+                $sort_order[$key] = $this->config->get('total_' . $value['code'] . '_sort_order');
+            }
 
-					// We have to put the totals in an array so that they pass by reference.
-					$this->{'model_extension_total_' . $result['code']}->getTotal($total_data);
-				}
-			}
+            array_multisort($sort_order, SORT_ASC, $results);
 
-			$sort_order = array();
+            foreach ($results as $result) {
+                if ($this->config->get('total_' . $result['code'] . '_status')) {
+                    $this->load->model('extension/total/' . $result['code']);
 
-			foreach ($totals as $key => $value) {
-				$sort_order[$key] = $value['sort_order'];
-			}
+                    // We have to put the totals in an array so that they pass by reference.
+                    $this->{'model_extension_total_' . $result['code']}->getTotal($total_data);
+                }
+            }
 
-			array_multisort($sort_order, SORT_ASC, $totals);
-		}
+            $sort_order = array();
 
-		$data['text_items'] = $this->cart->countProducts();
+            foreach ($totals as $key => $value) {
+                $sort_order[$key] = $value['sort_order'];
+            }
 
-		$this->load->model('tool/image');
-		$this->load->model('tool/upload');
+            array_multisort($sort_order, SORT_ASC, $totals);
+        }
 
-		$data['products'] = array();
+        $data['text_items'] = $this->cart->countProducts();
 
-		foreach ($this->cart->getProducts() as $product) {
-			if ($product['image']) {
-				$image = $this->model_tool_image->resize($product['image'], $this->config->get('theme_' . $this->config->get('config_theme') . '_image_cart_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_cart_height'));
-			} else {
-				$image = '';
-			}
+        $this->load->model('tool/image');
+        $this->load->model('tool/upload');
 
-			$option_data = array();
+        $data['products'] = array();
 
-			foreach ($product['option'] as $option) {
-				if ($option['type'] != 'file') {
-					$value = $option['value'];
-				} else {
-					$upload_info = $this->model_tool_upload->getUploadByCode($option['value']);
+        foreach ($this->cart->getProducts() as $product) {
+            if ($product['image']) {
+                $image = $this->model_tool_image->resize($product['image'], $this->config->get('theme_' . $this->config->get('config_theme') . '_image_cart_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_cart_height'));
+            } else {
+                $image = '';
+            }
 
-					if ($upload_info) {
-						$value = $upload_info['name'];
-					} else {
-						$value = '';
-					}
-				}
+            $option_data = array();
 
-				$option_data[] = array(
-					'name'  => $option['name'],
-					'value' => (utf8_strlen($value) > 20 ? utf8_substr($value, 0, 20) . '..' : $value),
-					'type'  => $option['type']
-				);
-			}
+            foreach ($product['option'] as $option) {
+                if ($option['type'] != 'file') {
+                    $value = $option['value'];
+                } else {
+                    $upload_info = $this->model_tool_upload->getUploadByCode($option['value']);
 
-			// Display prices
-			if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
-				$unit_price = $this->tax->calculate($product['price'], $product['tax_class_id'], $this->config->get('config_tax'));
-				
-				$price = $this->currency->format($unit_price, $this->session->data['currency']);
-				$total = $this->currency->format($unit_price * $product['quantity'], $this->session->data['currency']);
-			} else {
-				$price = false;
-				$total = false;
-			}
+                    if ($upload_info) {
+                        $value = $upload_info['name'];
+                    } else {
+                        $value = '';
+                    }
+                }
 
-			$data['products'][] = array(
-				'cart_id'   => $product['cart_id'],
-				'thumb'     => $image,
-				'name'      => $product['name'],
-				'model'     => $product['model'],
-				'option'    => $option_data,
-				'recurring' => ($product['recurring'] ? $product['recurring']['name'] : ''),
-				'quantity'  => $product['quantity'],
-				'price'     => $price,
-				'total'     => $total,
-				'href'      => $this->url->link('product/product', 'product_id=' . $product['product_id'])
-			);
-		}
+                $option_data[] = array(
+                    'name'  => $option['name'],
+                    'value' => (utf8_strlen($value) > 20 ? utf8_substr($value, 0, 20) . '..' : $value),
+                    'type'  => $option['type']
+                );
+            }
 
-		// Gift Voucher
-		$data['vouchers'] = array();
+            // Display prices
+            if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
+                $unit_price = $this->tax->calculate($product['price'], $product['tax_class_id'], $this->config->get('config_tax'));
 
-		if (!empty($this->session->data['vouchers'])) {
-			foreach ($this->session->data['vouchers'] as $key => $voucher) {
-				$data['vouchers'][] = array(
-					'key'         => $key,
-					'description' => $voucher['description'],
-					'amount'      => $this->currency->format($voucher['amount'], $this->session->data['currency'])
-				);
-			}
-		}
+                $price = $this->currency->format($unit_price, $this->session->data['currency']);
+                $total = $this->currency->format($unit_price * $product['quantity'], $this->session->data['currency']);
+            } else {
+                $price = false;
+                $total = false;
+            }
 
-		$data['totals'] = array();
+            $data['products'][] = array(
+                'cart_id'   => $product['cart_id'],
+                'thumb'     => $image,
+                'name'      => $product['name'],
+                'model'     => $product['model'],
+                'option'    => $option_data,
+                'recurring' => ($product['recurring'] ? $product['recurring']['name'] : ''),
+                'quantity'  => $product['quantity'],
+                'price'     => $price,
+                'total'     => $total,
+                'href'      => $this->url->link('product/product', 'product_id=' . $product['product_id'])
+            );
+        }
 
-		foreach ($totals as $total) {
-			$data['totals'][] = array(
-				'title' => $total['title'],
-				'text'  => $this->currency->format($total['value'], $this->session->data['currency']),
+        // Gift Voucher
+        $data['vouchers'] = array();
+
+        if (!empty($this->session->data['vouchers'])) {
+            foreach ($this->session->data['vouchers'] as $key => $voucher) {
+                $data['vouchers'][] = array(
+                    'key'         => $key,
+                    'description' => $voucher['description'],
+                    'amount'      => $this->currency->format($voucher['amount'], $this->session->data['currency'])
+                );
+            }
+        }
+
+        $data['totals'] = array();
+
+        foreach($this->cart->getProducts() as $giftItem){
+            if($giftItem['gift_summa'] == '0'){
+                $giftInCart = $giftItem['product_id'];
+            }
+        }
+        foreach($this->cart->getProducts() as $cartItem){
+            if($cartItem['product_id'] == $giftInCart && $totals[0]['value']<$this->getConfigSumm()[0]['price']){
+                $removeItem = $cartItem['cart_id'];
+            }
+            if($cartItem['product_id'] == $giftInCart && $totals[0]['value']>=$this->getConfigSumm()[0]['price']){
+                $giftIn = 'Y';
+            }
+        }
+        $data['remoteItem'] = $removeItem;
+        $data['giftIn'] = $giftIn;
+        foreach ($totals as $total) {
+            $data['totalValue'] = $total['value'];
+            $data['totals'][] = array(
+                'title' => $total['title'],
+                'text'  => $this->currency->format($total['value'], $this->session->data['currency']),
                 'value'  => $total['value'],
-			);
-		}
+            );
+        }
 
-		$data['cart'] = $this->url->link('checkout/cart');
-		$data['checkout'] = $this->url->link('checkout/checkout', '', true);
+        $data['giftSumm'] = $this->getConfigSumm()[0]['price'];
+        $data['cart'] = $this->url->link('checkout/cart');
+        $data['checkout'] = $this->url->link('checkout/checkout', '', true);
 
-		return $this->load->view('common/cart', $data);
-	}
+        return $this->load->view('common/cart', $data);
+    }
 
-	public function info() {
-		$this->response->setOutput($this->index());
-	}
+    public function info() {
+        $this->response->setOutput($this->index());
+    }
+    private function getConfigSumm() {
+        return $this->model_extension_gift_price_gift->getConfigSumm();
+    }
 }
---
+++ www/catalog/language/uk-ua/extension/gift_price/gift.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Опис подарку
+ * Посилання на автора https://opencartforum.com/profile/681757-fanatic/
+ * Автор fanatic
+ * Електронна пошта fanatiik0@gmail.com
+ */
+
+$_['button_cart_add'] = 'Отримати в подарунок';
+$_['text_bonus_h']  = 'Ваш бонус за сумою замовлення';
+$_['text_bonus_p'] = 'Виберіть собі подарунок! Доступно: ';
+$_['text_no_action'] = 'Ви вже обрали всі доступні подарунки для цієї акції.';
+
+$_['text_gift_next']  = 'Додайте ще товарів на <strong> %s </strong> і отримайте подарунок';
+$_['text_gift_select'] = 'Для суми <strong> %s </strong> обраний подарунок';
+$_['text_gift_on'] = 'Для суми <strong> %s </strong>доступні подарунки: ';
+
+$_['text_count_all_next']  = 'Додайте ще <strong> %s </strong> будь-яких товарів і отримайте подарунок';
+$_['text_count_all_select'] = 'Для <strong> %s </strong> товарів обраний подарунок';
+$_['text_count_all_on'] = 'У кошику <strong> %s </strong> товарів, і вам доступні подарунки: ';
+
+$_['text_count_select'] = 'Для товару <strong> %s </strong> обраний подарунок';
+$_['text_count_next']  = 'Для товару <strong> %s </strong> передбачено подарунок.  Додайте кількість <strong> %s </strong> і отримайте можливість вибрати подарунок. ';
+$_['text_count_on'] = 'У кошику &nbsp;<strong> %s </strong> товарів <strong> %s </strong> - вам доступні подарунки: ';
+
+$_['text_gift'] = 'Подарунок!';
+$_['text_action'] = 'Акція: ';
+$_['text_action_summa'] = 'Сума в кошику - ';
+$_['text_action_count'] = 'Кількість товару %s в кошику - ';
+$_['text_action_count_all'] = 'Кількість товарів в кошику - ';
+$_['text_shtuk'] = 'шт.';
+
+$_['text_product_one'] = 'товар';
+$_['text_product_two'] = 'товару';
+$_['text_product_three'] = 'товарів';
+
+$_['text_select'] = '-- Виберіть --';
+$_['text_option'] = 'Виберіть загальний товар';
+
+$_['button_cancel'] = 'Скасувати';
